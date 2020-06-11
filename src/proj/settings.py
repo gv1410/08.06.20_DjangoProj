7695af519ac0cdb7ea0ed7bc93b3bf18ae5197c9
gv1410 <gv_1410@tut.by>
2020-06-11 09:34:30 +0300

    11/06/20

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/home/gv1410/new_proj/static'
+STATIC_ROOT = '/home/gv1410/new/static'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = '/home/gv1410/new_proj/media'
+MEDIA_ROOT = '/home/gv1410/new/media'
