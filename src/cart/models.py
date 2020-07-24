cb476e20e691eeb3f57442ae856ca93a09b0e8e8
gv1410 <gv_1410@tut.by>
2020-07-24 14:41:35 +0300

    final

--- src/cart/models.py
+++ src/cart/models.py
@@ -43,13 +43,13 @@ class BookInCart(models.Model):
         verbose_name='Qantity',
         default=1
     )
-    '''
+    
     created = models.DateTimeField(
         "Created",
         auto_now=False,
         auto_now_add=True   
     )
-    '''
+    
     status = models.CharField(verbose_name='Статус', null=False, max_length=25, choices=CATEGORY, default='Ожидает подтверждения')
     def __str__(self):
         return f'Book #{self.book.pk} in cart #{self.cart.pk}'
